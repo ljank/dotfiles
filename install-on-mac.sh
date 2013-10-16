bbeb520a57ad52b4a3fc2802e37e22167f35cdc9
Lech Jankovski <godgard+github@gmail.com>
2013-10-16 15:51:37 +0300

    Add Package Control to ST3 config

--- install-on-mac.sh
+++ install-on-mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-DOT_HOME="${HOME}/dotfiles"
-SUBLIME_PATH="${HOME}/Library/Application Support/Sublime Text 3"
+DOT_HOME="$HOME/dotfiles"
+SUBLIME_PATH="$HOME/Library/Application Support/Sublime Text 3"
 
 WGET=`which wget`
 GIT=`which git`
@@ -21,24 +21,15 @@ if [[ ! -e $DOT_HOME ]]; then
 fi
 
 if [[ -e $SUBLIME_PATH ]]; then
-  SUBLIME_PKGS="${SUBLIME_PATH}/Packages"
-
   echo "Configuring Sublime.."
-  rm -frv "${SUBLIME_PKGS}/User"
-  ln -sv ~/dotfiles/sublime-text-3/ "${SUBLIME_PKGS}/User"
+  rm -frv "$SUBLIME_PATH/Packages/User"
+  ln -sv ~/dotfiles/sublime-text-3/ "$SUBLIME_PATH/Packages/User"
 
-  PACKAGE_CONTROL_PKG="${SUBLIME_PATH}/Installed Packages/Package Control.sublime-package"
+  PACKAGE_CONTROL_PKG="$SUBLIME_PATH/Installed Packages/Package Control.sublime-package"
   if [[ ! -e "$PACKAGE_CONTROL_PKG" ]]; then
     echo 'Installing Package Control..'
     $WGET "https://sublime.wbond.net/Package%20Control.sublime-package" -O "$PACKAGE_CONTROL_PKG"
     echo 'Package Control installed.'
-  fi
-
-  SODA_THEME_PKG="${SUBLIME_PKGS}/Theme - Soda"
-  if [[ ! -e "$SODA_THEME_PKG" ]]; then
-    echo 'Installing Theme - Soda..'
-    $GIT clone https://github.com/buymeasoda/soda-theme/ "$SODA_THEME_PKG"
-    echo 'Theme - Soda installed.'
   fi
 
   echo "Sublime configured."

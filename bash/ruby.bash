76a8fdb61557c776cb9bdf51b3cd7700b1412f47
Lech Jankovski <godgard+github@gmail.com>
2016-02-08 15:31:39 +0200

    Add alias for bundle exec

--- bash/ruby.bash
+++ bash/ruby.bash
@@ -4,6 +4,6 @@ export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_HEAP_FREE_MIN=500000
 
-alias z='bundle exec zeus'
+alias be='bundle exec'
 alias s='bundle exec spring'
 

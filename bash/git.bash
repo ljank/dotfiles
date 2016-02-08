ca7e66d7ec85644cdca179f309489882544e7e5b
Lech Jankovski <godgard+github@gmail.com>
2016-02-08 11:44:50 +0200

    Correct Git completion setup

--- bash/git.bash
+++ bash/git.bash
@@ -3,6 +3,6 @@ GIT_PS1_SHOWUNTRACKEDFILES=1
 GIT_PS1_SHOWSTASHSTATE=1
 GIT_PS1_SHOWUPSTREAM="auto"
 
-source ./git-completion.sh
+source "$(dirname $BASH_SOURCE)/git-completion.sh"
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]$(__git_ps1 " [%s]")\$ '
 

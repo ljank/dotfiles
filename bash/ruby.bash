e27b756d02fad890dfc619bd242d3943f60aa7fb
Lech Jankovski <godgard+github@gmail.com>
2016-02-08 11:45:15 +0200

    Remove rbenv setup

--- bash/ruby.bash
+++ bash/ruby.bash
@@ -7,21 +7,3 @@ export RUBY_HEAP_FREE_MIN=500000
 alias z='bundle exec zeus'
 alias s='bundle exec spring'
 
-# rbenv
-export PATH="/Users/ljank/.rbenv/shims:${PATH}"
-source "/usr/local/Cellar/rbenv/0.4.0/libexec/../completions/rbenv.bash"
-rbenv rehash 2>/dev/null
-rbenv() {
-  typeset command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  rehash|shell)
-    eval `rbenv "sh-$command" "$@"`;;
-  *)
-    command rbenv "$command" "$@";;
-  esac
-}
